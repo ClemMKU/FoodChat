6ce5fa8ed794b71866e9b9e6d476e8bbd3b71cd4
ClémentN <clement.niarquin@outlook.fr>
2017-09-29 14:05:23 +0200

    Début du projet

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+  wp_footer();
+?>
+
+
+<h1>footer</h1>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+
+ <head>
+  <meta charset="utf-8">
+  <link href="style.css" rel="stylesheet" media="all" type="text/css">
+
+ <?php
+  wp_head();
+?>
+
+</head>
+
+<body>
+
+<h1>header</h1>
